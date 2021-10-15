a2f2a5187cee989b7f0f6ab774dd1c73e324061b
heckfyxe <heckfyxe@gmail.com>
2021-10-15 15:40:28 +0300

    Added enter name after start

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,15 @@
 class WebhooksController < Telegram::Bot::UpdatesController
-  def start!
-    respond_with :message, text: 'Hello!'
+  def start!(*)
+    save_context :rename!
+    respond_with :message, text: 'Вставай, самурай! Введи свое имя и фамилию'
+  end
+
+  def rename!(firstname = nil, lastname = nil, *)
+    if !firstname && !lastname
+      save_context :rename!
+      respond_with :message, text: 'Введи имя и фамилию'
+    else
+      respond_with :message, text: "Теперь ты #{firstname} #{lastname}"
+    end
   end
 end
