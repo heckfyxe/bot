668d79cc05c3264ba84d975df5092fc3688c6a2c
heckfyxe <heckfyxe@gmail.com>
2021-10-15 17:39:35 +0300

    Fix

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -8,7 +8,10 @@ class WebhooksController < Telegram::Bot::UpdatesController
 
   def rename!(firstname = nil, lastname = nil, *)
     if firstname && lastname
-      User.upsert({ nickname: from[:username], firstname: firstname, lastname: lastname }, unique_by: :nickname)
+      user = User.find_or_initialize_by(nickname: from[:username])
+      user.firstname = firstname
+      user.lastname = lastname
+      user.save!
       respond_with :message, text: "Теперь ты #{firstname} #{lastname}"
     else
       save_context :rename!
