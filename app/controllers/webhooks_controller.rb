c35a0c98411619df0987bd0cf1e079542f4e2537
heckfyxe <heckfyxe@gmail.com>
2021-10-15 17:13:15 +0300

    Added user registration

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -8,7 +8,7 @@ class WebhooksController < Telegram::Bot::UpdatesController
 
   def rename!(firstname = nil, lastname = nil, *)
     if firstname && lastname
-      # User.upsert({ nickname: username, firstname: firstname, lastname: lastname }, unique_by: :nickname)
+      User.upsert({ nickname: from[:username], firstname: firstname, lastname: lastname }, unique_by: :nickname)
       respond_with :message, text: "Теперь ты #{firstname} #{lastname}"
     else
       save_context :rename!
